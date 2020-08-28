13145167c4c7cd00931f91df8692d2769b50b82a
leavemyyard <zhukovpavel2001@gmail.com>
2020-08-28 18:09:42 +0300

    0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="crypto_futures_py",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(),
     install_requires=["bitmex", "websocket_client"],
     # metadata to display on PyPI
